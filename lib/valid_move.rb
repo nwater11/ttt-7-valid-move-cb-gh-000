0dfadc50847c189c1c6221b59c6db94dbd5b2304
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-14 20:53:05 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == false && index.between(0, 8)
+  if position_taken?(board, index) == false && index.between?(0, 8)
        true
   else
     false
