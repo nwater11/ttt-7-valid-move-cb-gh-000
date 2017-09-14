3229754433bbb300d19451980f7ad75ca32b71ff
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-14 20:50:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,8 +2,8 @@
 
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == true
-    if index.between("0", "8")
+  if position_taken?(board, index) == false
+    if index.between(0, 8)
        true
     end
   else
